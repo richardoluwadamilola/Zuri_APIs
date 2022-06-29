2fb3c6e7d66cac13a84842ecd4654c4090a3e861
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-29 10:59:26 +0100

    working with APIs

---
+++ I4G000784J6J/links/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Link
+
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = "__all__"
+
+
+
+
---
+++ I4G000784J6J/links/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from links.serializers import LinkSerializer
+from .models import Link
+
+# Create your views here.
+class PostListApi(ListAPIView):
+  queryset = Link.objects.filter(active=True)
+  serializer_class = LinkSerializer  
+
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(DestroyAPIView):
+   queryset= Link.objects.filter(active=True)
+   serializer_class = LinkSerializer 
+
